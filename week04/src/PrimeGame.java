c5efc87c1c597516f4e9c018ce31d849f1c3cdc8
gyu0ne <gyuwon0316@gmail.com>
2025-09-04 21:44:58 +0900

    prime number v0.3

--- week04/src/PrimeGame.java
+++ week04/src/PrimeGame.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class PrimeGame {
     static void main(String[] args) {
+//        System.out.println(Math.sqrt(25.0));
+
         Scanner scanner = new Scanner(System.in);
         int counter = 0;
         int num = scanner.nextInt();
 
-        for(int i=2; i<num; i++){
+        for(int i=2; i<=Math.sqrt(num); i++){
             if(num%i == 0) {
                 counter++;
                 break;
