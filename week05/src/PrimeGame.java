395229094e2237e605624a0dde6a0a0bceb7ab13
gyu0ne <gyuwon0316@gmail.com>
2025-09-11 19:21:08 +0900

    prime number v0.5

--- week05/src/PrimeGame.java
+++ week05/src/PrimeGame.java
@@ -3,30 +3,24 @@ import java.util.Scanner;
 public class PrimeGame {
     // 숫자 하나를 받아서 해당 수가 소수면 true, 소수가 아니면 false return
     public static boolean isPrime(int n) {
-
-
+        if(n <= 1) {
+            return false;
+        }
+        else {
+            for(int i=2; i<=Math.sqrt(n); i++){
+                if(n%i == 0) {
+                    return false;
+                }
+            }
+        }
         return  true;
     }
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        boolean isPrime = true;
         int num = scanner.nextInt();
 
-        if(num <= 1) {
-            isPrime = false;
-        }
-        else {
-            for(int i=2; i<=Math.sqrt(num); i++){
-                if(num%i == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-        }
-
-        if(isPrime == true){
+        if(isPrime(num)){
             System.out.println(num + " is a prime number.");
         } else {
             System.out.println(num + " is not a prime number.");
