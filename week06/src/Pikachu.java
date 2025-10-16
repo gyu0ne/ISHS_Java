2563ec29e02a939e3de9bfc6d597280a5aad818a
gyu0ne <gyuwon0316@gmail.com>
2025-10-16 19:34:30 +0900

    v0.1 Pokemon Game

---
+++ week06/src/Pikachu.java
@@ -0,0 +1,12 @@
+public class Pikachu extends Pokemon {
+    public Pikachu(int hp) {
+        super("피카츄", hp); // 부모 클래스에 생성자 호출
+        System.out.println("PikaPika");
+    }
+
+    @Override
+    public void attack(Pokemon target) {
+        // System.out.println(); name is private
+        System.out.println(name + " -> " + target.getName() + "에게 백만볼트!!!!!!!!!!!!!!!!!"); // name is protected
+    }
+}
