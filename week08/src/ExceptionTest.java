d24dc572ad78c501c992f20195c8c31348774a5d
gyu0ne <gyuwon0316@gmail.com>
2025-10-30 20:23:30 +0900

    exception handling v0.2

--- week08/src/ExceptionTest.java
+++ week08/src/ExceptionTest.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 public class ExceptionTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Input Numerator");
+        System.out.print("Input Numerator : ");
+//        double numerator = scanner.nextDouble();
         int numerator = scanner.nextInt();
         System.out.println();
-        System.out.print("Input Denominator");
+        System.out.print("Input Denominator : ");
+//        double denominator = scanner.nextDouble();
         int denominator = scanner.nextInt();
 
-        System.out.println(numerator/denominator);
+        if (denominator == 0) {
+            System.out.println("분모는 0이 될 수 없으셈");
+        } else {
+            System.out.println(numerator/denominator);
+        }
     }
 }
