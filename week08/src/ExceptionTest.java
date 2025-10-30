2cea115a0f0bafa1f144d9203af1c1f1dffb3aa6
gyu0ne <gyuwon0316@gmail.com>
2025-10-30 20:38:16 +0900

    exception handling v0.3

--- week08/src/ExceptionTest.java
+++ week08/src/ExceptionTest.java
@@ -1,15 +1,26 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Input Numerator : ");
+
+        int numerator = 0;
+        int denominator = 0;
+
+        try {
+            System.out.print("Input Numerator : ");
 //        double numerator = scanner.nextDouble();
-        int numerator = scanner.nextInt();
-        System.out.println();
-        System.out.print("Input Denominator : ");
+            numerator = scanner.nextInt();
+            System.out.println();
+            System.out.print("Input Denominator : ");
 //        double denominator = scanner.nextDouble();
-        int denominator = scanner.nextInt();
+            denominator = scanner.nextInt();
+
+            System.out.println(numerator/denominator);
+        } catch (InputMismatchException err) {
+            System.out.println("숫자로 입력하셈");
+        }
 
         if (denominator == 0) {
             System.out.println("분모는 0이 될 수 없으셈");
